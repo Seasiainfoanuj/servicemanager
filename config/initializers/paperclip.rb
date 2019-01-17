10cdbbc99c96a8dcaa56288c63efe673bf816118
Tim Hemi (Ops) <tim@reinteractive.net>
2019-01-17 16:51:52 +1100

    remove leaduing slash for paparclip path

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -6,7 +6,7 @@ if Rails.env.staging? || Rails.env.production?
       provider: 'AWS',
       region: ENV['AWS_S3_REGION'] || 'ap-southeast-2'
     },
-    path: "/uploads/:class/:attachment/:id_partition/:style/:filename",
+    path: "uploads/:class/:attachment/:id_partition/:style/:filename",
     fog_directory: ENV['AWS_S3_BUCKET'],
     fog_public: true,
     fog_host: ENV['RAILS_ASSET_HOST']
